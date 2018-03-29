4d4073f0a56af7c756dd228ba657f5c146b16bb1
Kazi Asfar Ahmed <asfar_666@live.com>
2018-03-29 04:16:31 +0000

    A12  Same code but works now

--- index.js
+++ index.js
@@ -7,14 +7,11 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', function(socket){
-  //console.log('a user connected');
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
-  });
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
   });
 });
 
